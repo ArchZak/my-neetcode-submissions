3207fa8aeb9ea45c7a283db6674878c48df45fe6
ArchZak <izear21@gmail.com>
2026-04-21 09:55:20 -0400

    Add: majority-element-ii - submission-3

---
+++ Data Structures & Algorithms/majority-element-ii/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        import math
+        tracker = {}
+
+        for num in nums:
+            tracker[num] = tracker.get(num,0)+1
+
+
+        return [key for key, value in tracker.items() if value > len(nums)//3]
