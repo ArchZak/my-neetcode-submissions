de1165dc82a86fc9af73d1b70dce000c0b3a5ca6
ArchZak <izear21@gmail.com>
2026-04-24 00:16:49 -0400

    Update: find-the-town-judge - submission-2

--- Data Structures & Algorithms/find-the-town-judge/submission-2.py
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.py
@@ -1,20 +1,5 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        #array of people where one is secretly the judge
-        #town judge trusts nobody
-        #everyone trusts the town judge
-        #only one person is 1 and 2
-
-        #given n where n is n people
-        #given array where trust[i] is [ai,bi] where ai trusts bi
-
-        #return label of town judge or -1 if he doesnt exist
-
-        #should loop through array of trust
-        #have helper tracker that is n+1 long
-        #++ 1 to index that is trusted and --1 if person trusts
-        #return the first index we find that is == n-1 (everyone but himself)
-
         tracker = [0]*(n+1)
         for ai, bi in trust:
             tracker[ai]-=1
