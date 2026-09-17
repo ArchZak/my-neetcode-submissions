acb6f143ba18e403427ba5f51169bb2d63e1abf1
ArchZak <izear21@gmail.com>
2026-09-17 12:59:31 -0400

    Add: meeting-schedule - submission-15

---
+++ Data Structures & Algorithms/meeting-schedule/submission-15.py
@@ -0,0 +1,47 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        #input: list of [int,int] not ordered
+        #output: can person attend all meetings? (boolean)
+
+        #array of [[starti,endi]] where start < end, can person attend all meetings
+        #intervals can be given in any order 
+
+        #array can be empty
+
+        #split intervals into two arrays of start and finish after sorting it
+        #then need pointers on start and end array
+        #basically just need to find an end bigger than start
+        #starting at 0, 0 -> move start up one and if bigger than old end, move end up 1
+        #if we come across an instance where end_i-1 < start_i, then we return false
+
+        #interval_sort = [(x.start, x.end) for x in intervals]
+        #.sort()
+        #start = get start, end = get ends
+        #startptr, endptr = 1, 0
+        #while start < len start:
+        #   if start val < end val:
+        #       return false
+        #   end++, start++
+        #return true
+
+        interval_sort = [(x.start, x.end) for x in intervals]
+        interval_sort.sort()
+        starts, ends = [x[0] for x in interval_sort], [x[1] for x in interval_sort]
+
+        startptr, endptr = 1, 0
+        while startptr < len(starts):
+            if starts[startptr] < ends[endptr]:
+                return False
+            startptr+=1
+            endptr+=1
+
+        return True
+        
